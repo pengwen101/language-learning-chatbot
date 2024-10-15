7566720fb94542bfa70d941f55ad9184e1effa72
pengwen101 <111033605+pengwen101@users.noreply.github.com>
2024-10-15 19:57:01 +0700

    final

---
+++ final_uts/riasec_final.py
@@ -0,0 +1,105 @@
+from llama_index.llms.ollama import Ollama
+from llama_index.readers.file import CSVReader
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core.node_parser import SentenceWindowNodeParser, SentenceSplitter
+from llama_index.core.tools import QueryEngineTool
+from llama_index.core.query_engine import RouterQueryEngine
+from llama_index.core import Settings
+from llama_index.llms.openai import OpenAI
+from pathlib import Path
+from llama_index.core import PromptTemplate
+from llama_index.core.llms import ChatMessage, MessageRole
+from llama_index.core.chat_engine import CondensePlusContextChatEngine
+from llama_index.core.memory import ChatMemoryBuffer
+from llama_index.core.objects import ObjectIndex
+from llama_index.core.query_engine import ToolRetrieverRouterQueryEngine
+import streamlit as st
+import os
+from collections import defaultdict
+import pandas as pd
+
+riasec_result_data = pd.read_csv('../answers/riasec_assessment_answer.csv')
+riasec_result_key_values = [{row['Type']: row['Total Score']} for index, row in riasec_result_data.iterrows()]
+top_3 = sorted(riasec_result_key_values, key=lambda x: list(x.values())[0], reverse=True)[:3]
+print(top_3)
+
+system_prompt = f"""
+You are a multi-lingual expert system who has knowledge, based on 
+real-time data. You will always try to be helpful and try to help them 
+answering their question. If you don't know the answer, say that you DON'T
+KNOW.
+
+Jawablah semua dalam Bahasa Indonesia.
+Anda adalah asesor yang memiliki tugas untuk membantu pengguna untuk mencari karir berdasarkan aspek teratas holland personality yang dimiliki oleh pengguna. Aspek teratas yang dimiliki user adalah {top_3[0]}, {top_3[1]}, {top_3[2]}. JANGAN SEBUTKAN ASPEK LAIN SELAIN TIGA ASPEK TERSEBUT. Gunakan kata-kata yang FRIENDLY, mudah dimengerti dan RAMAH serta gaul sehingga mudah untuk dibaca anak muda.
+
+Percakapan sejauh ini:
+"""
+
+Settings.llm = Ollama(model="llama3.1:latest", base_url="http://127.0.0.1:11434", system_prompt=system_prompt) 
+Settings.embed_model = OllamaEmbedding(base_url="http://127.0.0.1:11434", model_name="mxbai-embed-large:latest") 
+
+holland_docs = SimpleDirectoryReader("../docs").load_data()
+docs_index = VectorStoreIndex.from_documents(holland_docs)
+
+retriever = docs_index.as_retriever()
+
+condense_question_prompt = """
+Diberikan suatu percapakan (antara manusia dan asisten) dan sebuah pesan lanjutan dari manusia. Ubah pesan lanjutan menjadi pertanyaan independen yang mencakup semua konteks relevan
+dari percakapan sebelumnya.
+
+<Chat History>
+{chat_history}
+
+<Follow Up Message>
+{question}
+
+<Standalone question>
+"""
+
+
+context_question_prompt = "Anda adalah ahli konsultasi karir yang dapat memberikan rekomendasi pekerjaan berdasarkan hasil tes RIASEC pengguna, yaitu " + list(top_3[0].keys())[0] + ", " +  list(top_3[1].keys())[0] + ", " + list(top_3[2].keys())[0] + ". Ini adalah dokumen yang mungkin relevan terhadap konteks:\n\n {context_str} \n\nInstruksi: Gunakan riwayat obrolan sebelumnya, atau konteks di atas, untuk berinteraksi dan membantu pengguna. Berikan analisis serta 5 pekerjaan yang cocok untuk user."
+
+print("context: ", context_question_prompt)
+print("system: ", system_prompt)
+st.title("Rekomendasi Karir Berdasarkan Hasil Tes RIASEC 💼")
+# st.write("Lorem ipsum dolor sit amet")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "assistant",
+         "content": "Halo, apakah anda siap untuk mendapatkan rekomendasi karir?"}
+    ]
+
+if "chat_engine" not in st.session_state:
+
+    memory = ChatMemoryBuffer.from_defaults(token_limit=50384)
+    st.session_state.chat_engine = CondensePlusContextChatEngine(
+    retriever=retriever,
+    condense_prompt=condense_question_prompt,
+    context_prompt = context_question_prompt,
+    memory=memory,
+    llm=Settings.llm,
+    verbose=True
+)
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt:= st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        with st.spinner("Berpikir..."):
+            response_stream = st.session_state.chat_engine.chat(prompt)
+            st.markdown(response_stream)
+
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response_stream})
