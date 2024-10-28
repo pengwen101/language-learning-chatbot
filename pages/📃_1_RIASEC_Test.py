9fb670a447be64d1ba50eecc0f797645a3638a62
pengwen101 <111033605+pengwen101@users.noreply.github.com>
2024-10-28 20:19:58 +0700

    update

--- pages/📃_1_RIASEC_Test.py
+++ pages/📃_1_RIASEC_Test.py
@@ -11,50 +11,55 @@ with st.form("riasec_form"):
     st.write("Jawablah pertanyaan berikut dengan skala 1-5: \n")
     st.write("**(1 = Sangat Tidak Yakin, 2 = Tidak Yakin, 3 = Netral, 4 = Yakin, 5 = Sangat Yakin)**")
 
-    if "answers" not in st.session_state:
+    if 'answers' not in st.session_state:
         st.session_state.answers = {}
     
-        st.subheader("Questions:")
-
-        for index, row in questions_df.iterrows():
-            question = row['Question']
-            st.session_state.answers[f"answer_{index}"] = st.slider(f"{index+1}. {question}", min_value=1, max_value=5, value=1, format="%d", help="1 = Sangat Tidak Yakin, 2 = Tidak Yakin, 3 = Netral, 4 = Yakin, 5 = Sangat Yakin")
-
+    st.subheader("Questions:")
+    
+    for index, row in questions_df.iterrows():
+        question = row['Question']
+        st.session_state.answers[f"answer_{index}"] = st.slider(f"{index+1}. {question}", min_value=1, max_value=5, format="%d", help="1 = Sangat Tidak Yakin, 2 = Tidak Yakin, 3 = Netral, 4 = Yakin, 5 = Sangat Yakin")
+    
     st.markdown("""
-    <style>
-    div.stButton > button {
-        display: block;
-        margin: 0 auto;
-    }
-    </style>
-    """, unsafe_allow_html=True)
+            <style>
+            div.stButton > button {
+                display: block;
+                margin: 0 auto;
+            }
+            </style>
+            """, unsafe_allow_html=True)
+    
+    submitted = st.form_submit_button("Submit")
 
-    st.session_state.submitted = st.form_submit_button("Submit")
-
-if st.session_state.submitted:
+if submitted:
     total_scores = {type_: 0 for type_ in questions_df['Type'].unique()}
     for index in range(len(questions_df)):
         type_ = questions_df.loc[index, 'Type']
         score = st.session_state.answers[f'answer_{index}']
         total_scores[type_] += score
-
+    
+    
     # Menyimpan hasil ke file CSV baru
     total_scores_df = pd.DataFrame(total_scores.items(), columns=['Type', 'Total Score'])
-    total_scores_df.to_csv('./answers/riasec_assessment_answer.csv', index=False)
+    print(total_scores_df)
+    file_path = './answers/riasec_assessment_answer.csv'
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    total_scores_df.to_csv(file_path, index=False)
     st.success("Jawaban anda telah berhasil disimpan!")
+    
     st.session_state.top_3 = total_scores_df.sort_values(by='Total Score', ascending=False).head(3)
+    # Displaying the results in a box with st.markdown and table
 
-
-if "top_3" in st.session_state:
-     # Displaying the results in a box with st.markdown and table
+if 'top_3' in st.session_state:
     st.markdown(
-        f"""
-        <div style="border: 2px solid #4CAF50; padding: 10px; border-radius: 10px; background-color: #f9f9f9;">
-            <h3 style="color: #4CAF50;">Hasil Tes Anda</h3>
-            <h5>{st.session_state.top_3.iloc[0, 0]}</h5>
-            <h5>{st.session_state.top_3.iloc[1, 0]}</h5>
-            <h5>{st.session_state.top_3.iloc[2, 0]}</h5>
-        </div>
-        """, unsafe_allow_html=True
+            f"""
+            <div style="border: 2px solid #4CAF50; padding: 10px; border-radius: 10px; background-color: #f9f9f9;">
+                <h3 style="color: #4CAF50;">Hasil Tes Anda</h3>
+                <h5>{st.session_state.top_3.iloc[0, 0]}</h5>
+                <h5>{st.session_state.top_3.iloc[1, 0]}</h5>
+                <h5>{st.session_state.top_3.iloc[2, 0]}</h5>
+            </div>
+            """, unsafe_allow_html=True
     )
     
